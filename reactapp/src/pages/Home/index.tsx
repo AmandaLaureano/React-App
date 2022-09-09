f87eb434a541b6e0ffede3123b8216c9767bb7cb
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2022-09-09 13:25:27 -0300

    ajustes

--- reactapp/src/pages/Home/index.tsx
+++ reactapp/src/pages/Home/index.tsx
@@ -1,11 +1,15 @@
 import './styles.css';
 
+import { Card } from '../../components/Card';
+
 export function Home() {
   return (
     <div className='container'>
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
+
+      <Card name='Rodrigo' time="10:55:25"/>
     </div>
   )
 }
