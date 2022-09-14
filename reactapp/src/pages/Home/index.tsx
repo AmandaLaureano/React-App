2ac0c5cc369dd161e78947fe6d6da88d582614c3
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2022-09-14 11:14:54 -0300

    desenvolvimento final

--- reactapp/src/pages/Home/index.tsx
+++ reactapp/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css';
 
 import Card from '../../components/Card';
@@ -6,6 +6,7 @@ import Card from '../../components/Card';
 export function Home() {
   const [studentName, setStudentName] = useState('');
   const [students, setStudents] = useState([]);
+  const [user, setUser]= useState({name: '', avatar: ''});
 
   function handleAddStudent(){
     const newStudent = {
@@ -20,15 +21,37 @@ export function Home() {
     setStudents(previousState => [...previousState, newStudent]);
   }
 
+  useEffect(()=> {
+    async function fetchData(){
+      const response = await fetch('https://api.github.com/users/amandalaureano')
+      const data = await response.json();
+      console.log("DADOS -->", data);
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      });
+    }
+
+  fetchData();
+  }, [])
+  
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>Amanda</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
 
+        </div>
+      </header>
+      
       <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)} />
       <button type="button" onClick={handleAddStudent}>Adicionar</button>
 
     {
-      students.map(student => <Card name={student.name} time={student.time}/>)
+      students.map(student => <Card name={student.name} time={student.time} key={student.time}/>)
     }
     </div>
   )
